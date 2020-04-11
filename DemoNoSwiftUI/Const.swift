713458fb50b35558fa4c4bb53852631ea4b0baf4
wenluma <wenluma_521@163.com>
2020-04-11 14:36:20 +0800

    swift 常量声明

---
+++ DemoNoSwiftUI/Const.swift
@@ -0,0 +1,15 @@
+//
+//  Const.swift
+//  DemoNoSwiftUI
+//
+//  Created by miao gaoliang on 2020/4/11.
+//  Copyright © 2020 miao gaoliang. All rights reserved.
+//
+
+import Foundation
+
+// 声明常量
+public let kDuration = 0.3
+
+// 内部可见
+private let kVersion = 0.1.0
