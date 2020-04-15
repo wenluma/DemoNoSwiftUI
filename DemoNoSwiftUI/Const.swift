e58a08f94510b07d469166db59de0667bd1eb809
wenluma <wenluma_521@163.com>
2020-04-15 09:09:19 +0800

    类型别名，可以做平台兼容使用，编译指令集，应该也可以

--- DemoNoSwiftUI/Const.swift
+++ DemoNoSwiftUI/Const.swift
@@ -17,3 +17,11 @@ private let kVersion = "0.1.0"
 
 let f1 = ceil(0.1) // 1 向上取整
 let f2 = floor(0.1) // 0 向下取整
+
+#if os(iOS)
+    import UIKit
+    typealias OSApplication = UIApplication
+#elseif os(macOS)
+    import Cocoa
+    typealias OSApplication = NSApplication
+#endif
