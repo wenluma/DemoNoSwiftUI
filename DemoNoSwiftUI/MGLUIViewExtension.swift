c296e626d7620e96775c0ae4784b28f93a96a388
wenluma <wenluma_521@163.com>
2020-04-17 09:33:54 +0800

    添加view 的 frame extension, x, y, top, left, bottom, right, offset

---
+++ DemoNoSwiftUI/MGLUIViewExtension.swift
@@ -0,0 +1,227 @@
+//
+//  MGLUIViewExtension.swift
+//  DemoNoSwiftUI
+//
+//  Created by miao gaoliang on 2020/4/17.
+//  Copyright © 2020 miao gaoliang. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+extension UIView {
+//    MARK: x, y, width, heigh
+    var mgl_x: CGFloat {
+        get {
+            frame.origin.x
+        }
+        set {
+            let rect = frame
+            frame = CGRect(x: newValue, y: rect.origin.y, width: rect.width, height: rect.height)
+        }
+    }
+    
+    var mgl_y: CGFloat {
+        get {
+            frame.origin.y
+        }
+        set {
+            let rect = frame
+            frame = CGRect(x: rect.origin.x , y: newValue, width: rect.width, height: rect.height)
+        }
+    }
+    
+    var mgl_width: CGFloat {
+        get {
+            return frame.width
+        }
+        set {
+            let rect = frame
+            frame = CGRect(x: rect.origin.x, y: rect.origin.y, width: newValue, height: rect.height)
+        }
+    }
+    
+    var mgl_heigh: CGFloat {
+        get {
+            return frame.height
+        }
+        set {
+            let rect = frame
+            frame = CGRect(x: rect.origin.x, y: rect.origin.y, width: rect.width, height: newValue)
+        }
+    }
+    
+    var mgl_center: CGPoint {
+        get {
+            center
+        }
+        set {
+            center = newValue
+        }
+    }
+    
+    var mgl_centerX: CGFloat {
+        get {
+            center.x
+        }
+        set {
+            center = CGPoint(x: newValue, y: center.y)
+        }
+    }
+    
+    var mgl_centerY: CGFloat {
+        get {
+            center.y
+        }
+        set {
+            center = CGPoint(x:center.x , y: newValue)
+        }
+    }
+    
+    var mgl_size: CGSize {
+        get {
+            frame.size
+        }
+        set {
+            frame =  CGRect(x: mgl_x, y: mgl_y, width: newValue.width, height: newValue.height)
+        }
+    }
+    
+    var mgl_frame: CGRect {
+        get {
+            frame
+        }
+        set {
+            frame = newValue
+        }
+    }
+    
+//    MARK: top, left, bottom, right
+    var mgl_top: CGFloat {
+        get {
+            mgl_y
+        }
+        set {
+            mgl_y = newValue
+        }
+    }
+    
+    var mgl_left: CGFloat {
+        get {
+            mgl_x
+        }
+        set {
+            mgl_x = newValue
+        }
+    }
+    
+    var mgl_bottom: CGFloat {
+        get {
+            guard let superview = superview else {
+                fatalError("must add to super view")
+            }
+            return superview.bounds.height - frame.height - mgl_y
+
+        }
+        set {
+            guard let superview = superview else {
+                fatalError("must add to super view")
+            }
+            
+            mgl_y = superview.bounds.height - frame.height - newValue
+        }
+    }
+    
+    var mgl_right: CGFloat {
+        get {
+            guard let superview = superview else {
+                fatalError("must add to super view")
+            }
+            return superview.bounds.width - frame.width - mgl_x
+        }
+        set {
+            guard let superview = superview else {
+                fatalError("must add to super view")
+            }
+            mgl_x = superview.bounds.width - frame.width - newValue
+        }
+    }
+//    MARK: offsetx, offsetY, offsetPoint
+    func mgl_offset(x dx: CGFloat) -> Void {
+        let rect = frame
+        frame = rect.offsetBy(dx: dx, dy: 0)
+    }
+    
+    func mgl_offset(y dy: CGFloat) -> Void {
+        let rect = frame
+        frame = rect.offsetBy(dx: 0, dy: dy)
+    }
+    
+    func mgl_offset(dx: CGFloat ,dy: CGFloat) -> Void {
+        let rect = frame
+        frame = rect.offsetBy(dx: dx, dy: dy)
+    }
+    
+    func mgl_offset(point offsetPoint: CGPoint) -> Void {
+        let rect = frame
+        frame = rect.offsetBy(dx: offsetPoint.x, dy: offsetPoint.y)
+    }
+    
+//    MARK: edge frame
+    func mgl_edgeInsets(forFrame edgeInsets: UIEdgeInsets) -> Void {
+        let rect = frame
+        frame = rect.inset(by: edgeInsets)
+    }
+    
+    func mgl_edgeInset(forFrameLR dx: CGFloat) -> Void {
+        let rect = frame
+        frame = rect.insetBy(dx: dx, dy: 0)
+    }
+    
+    func mgl_edgeInset(forFrameTB dy: CGFloat) -> Void {
+        let rect = frame
+        frame = rect.insetBy(dx: 0, dy: dy)
+    }
+    
+//    MARK: equal super, center in super
+    func mgl_eqToSuperView() -> Void {
+        guard let superview = superview else {
+            fatalError("must add to super view")
+        }
+        frame = superview.bounds
+    }
+    
+    func mgl_centerInSuperView() -> Void {
+        guard let superview = superview else {
+            fatalError("must add to super view")
+        }
+        center = CGPoint(x: superview.bounds.midX, y: superview.bounds.midY)
+    }
+    
+    /// 扩展view
+    /// - Parameter edgeDx: newWidth = oldWidth -  edgeDx * 2
+    /// - Returns:edgeDx > 0, 变小， edgeDx < 0, 变大
+    func mgl_edgeInSuperView(withLR dx: CGFloat) -> Void {
+        mgl_edgeInSuperView(withLR: dx, withTB: 0)
+    }
+    
+    func mgl_edgeInSuperView(withTB dy: CGFloat) -> Void {
+        mgl_edgeInSuperView(withLR: 0, withTB: dy)
+    }
+    
+    func mgl_edgeInSuperView(withLR dx: CGFloat, withTB dy: CGFloat) -> Void {
+        guard let superview = superview else {
+            fatalError("must add to super view")
+        }
+        let rect = superview.bounds
+        frame = rect.insetBy(dx: dx, dy: dy)
+    }
+    
+    func mgl_edgeInSuperView(withPadding edgeInsets: UIEdgeInsets) -> Void {
+        guard let superview = superview else {
+            fatalError("must add to super view")
+        }
+        let rect = superview.bounds
+        frame = rect.inset(by: edgeInsets)
+    }
+}
--- DemoNoSwiftUIUITests/DemoNoSwiftUIUITests.swift
+++ DemoNoSwiftUIUITests/DemoNoSwiftUIUITests.swift
@@ -7,6 +7,7 @@
 //
 
 import XCTest
+import DemoNoSwiftUI
 
 class DemoNoSwiftUIUITests: XCTestCase {
 
@@ -28,6 +29,124 @@ class DemoNoSwiftUIUITests: XCTestCase {
         let app = XCUIApplication()
         app.launch()
 
+        let originRect = CGRect(x: 0, y: 0, width: 50, height: 50)
+        let supV = UIView(frame: CGRect(x: 0, y: 0, width: 100, height: 100))
+        let viewa = UIView(frame: originRect)
+        supV.addSubview(viewa)
+        viewa.mgl_x = 10
+        
+        assert(viewa.mgl_frame.equalTo(CGRect(x: 10, y: 0, width: 50, height: 50)))
+        assert(viewa.mgl_x == 10)
+        
+        viewa.mgl_x = 0
+        assert(viewa.mgl_frame.equalTo(CGRect(x: 0, y: 0, width: 50, height: 50)))
+
+        viewa.mgl_y = 10
+
+        assert(viewa.mgl_frame.equalTo(CGRect(x: 0, y: 10, width: 50, height: 50)))
+        assert(viewa.mgl_y == 10)
+        
+        viewa.mgl_y = 0
+        assert(viewa.mgl_frame.equalTo(CGRect(x: 0, y: 0, width: 50, height: 50)))
+
+        
+        viewa.mgl_width = 60
+        assert(viewa.mgl_frame.equalTo(CGRect(x: 0, y: 0, width: 60, height: 50)))
+        assert(viewa.mgl_width == 60)
+
+        viewa.mgl_heigh = 70
+        assert(viewa.mgl_frame.equalTo(CGRect(x: 0, y: 0, width: 60, height: 70)))
+        assert(viewa.mgl_heigh == 70)
+
+        viewa.mgl_x = 10
+        assert(viewa.mgl_frame.equalTo(CGRect(x: 10, y: 0, width: 60, height: 70)))
+        assert(viewa.mgl_heigh == 70)
+        assert(viewa.mgl_x == 10)
+        assert(viewa.mgl_width == 60)
+
+        viewa.mgl_y = 10
+        assert(viewa.mgl_frame.equalTo(CGRect(x: 10, y: 10, width: 60, height: 70)))
+        assert(viewa.mgl_heigh == 70)
+        assert(viewa.mgl_x == 10)
+        assert(viewa.mgl_y == 10)
+        assert(viewa.mgl_width == 60)
+        
+        viewa.frame = CGRect(x: 0, y: 0, width: 50, height: 50)
+        assert(viewa.mgl_center == CGPoint(x: 25, y: 25))
+        assert(viewa.mgl_center == viewa.center)
+        
+        assert(viewa.mgl_centerX == 25)
+        assert(viewa.mgl_centerY == 25)
+
+        viewa.mgl_offset(x: 10)
+        assert(viewa.mgl_centerX == 25 + 10)
+
+        viewa.mgl_offset(y: 10)
+        assert(viewa.mgl_centerY == 25 + 10)
+        assert(viewa.mgl_center == CGPoint(x: 25 + 10, y: 25 + 10))
+
+        viewa.center = CGPoint(x: 25, y: 25)
+        assert(viewa.frame == CGRect(x: 0, y: 0, width: 50, height: 50))
+        
+        viewa.mgl_offset(point: CGPoint(x: 10, y: 10))
+        assert(viewa.mgl_center == CGPoint(x: 25 + 10, y: 25 + 10))
+
+        assert(viewa.mgl_left == 10)
+        assert(viewa.mgl_top == 10)
+        
+        viewa.mgl_right = 10
+        assert(viewa.frame == CGRect(x: 100 - 50 - 10, y: 10, width: 50, height: 50))
+        
+        viewa.mgl_size = CGSize(width: 60, height: 60)
+        assert(viewa.frame == CGRect(x: 100 - 50 - 10, y: 10, width: 60, height: 60))
+
+        viewa.mgl_frame = originRect
+        assert(viewa.frame == originRect)
+
+        viewa.mgl_bottom = 10
+        assert(viewa.frame != CGRect(x: 0, y: 100 - 50 - 10, width: 60, height: 60))
+        assert(viewa.frame == CGRect(x: 0, y: 100 - 50 - 10, width: 50, height: 50))
+
+        viewa.mgl_top = 10
+        assert(viewa.frame == CGRect(x: 0, y: 10, width: 50, height: 50))
+
+        viewa.mgl_top = 0
+        assert(viewa.frame == CGRect(x: 0, y: 0, width: 50, height: 50))
+
+        viewa.mgl_edgeInsets(forFrame: UIEdgeInsets(top: 10, left: 10, bottom: 10, right: 10))
+        assert(viewa.frame == CGRect(x: 10, y: 10, width: 30, height: 30))
+
+        viewa.mgl_frame = originRect
+        viewa.mgl_edgeInset(forFrameLR: 10)
+        assert(viewa.frame == CGRect(x: 10, y: 0, width: 30, height: 50))
+        
+        viewa.mgl_edgeInset(forFrameTB: 10)
+        assert(viewa.frame == CGRect(x: 10, y: 10, width: 30, height: 30))
+
+        
+        viewa.mgl_edgeInSuperView(withLR: 10)
+        assert(viewa.frame == CGRect(x: 10, y: 0, width: 80, height: 100))
+
+        viewa.mgl_edgeInSuperView(withTB: 10)
+        assert(viewa.frame == CGRect(x: 0, y: 10, width: 100, height: 80))
+        
+        
+        viewa.mgl_edgeInSuperView(withLR: 10, withTB: 10)
+        assert(viewa.frame == CGRect(x: 10, y: 10, width: 80, height: 80))
+
+        viewa.mgl_edgeInSuperView(withPadding: UIEdgeInsets(top: 10, left: 20, bottom: 20, right: 30))
+        assert(viewa.frame == CGRect(x: 20, y: 10, width: 100-30-20, height:100-20-10))
+
+        viewa.mgl_centerInSuperView()
+        assert(viewa.center == CGPoint(x: 50, y: 50))
+
+        
+        
+//        print("mgl: \(viewa.frame)")
+//        print("mgl: \(viewa.mgl_x)")
+        
+        
+        
         // Use recording to get started writing UI tests.
         // Use XCTAssert and related functions to verify your tests produce the correct results.
     }
