82fe83fa44cd96affe59779cc3685da5b9c6ebbb
wenluma <wenluma_521@163.com>
2020-04-20 13:26:41 +0800

    string 获取 substring 的方式

--- DemoNoSwiftUI/MGLStringEx.swift
+++ DemoNoSwiftUI/MGLStringEx.swift
@@ -83,3 +83,26 @@ extension NSAttributedString {
         return self.boundingRect(with: CGSize(width: maxWidth, height: CGFloat.greatestFiniteMagnitude), options: [NSStringDrawingOptions.usesLineFragmentOrigin, .usesFontLeading], context: nil)
     }
 }
+
+extension String {
+    func index(from: Int) -> Index {
+        return self.index(startIndex, offsetBy: from)
+    }
+
+    func substring(from: Int) -> String {
+        let fromIndex = index(from: from)
+        return String(self[fromIndex...])
+    }
+
+    func substring(to: Int) -> String {
+        let toIndex = index(from: to)
+        return String(self[..<toIndex])
+    }
+
+    func substring(with r: Range<Int>) -> String {
+        let startIndex = index(from: r.lowerBound)
+        let endIndex = index(from: r.upperBound)
+        return String(self[startIndex..<endIndex])
+    }
+}
+
