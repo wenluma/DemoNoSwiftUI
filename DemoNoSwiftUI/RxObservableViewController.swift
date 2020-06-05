a0b28e62f4adbfb2fa5e2d16892172f4492fc1f6
wenluma <wenluma_521@163.com>
2020-06-05 11:42:52 +0800

    test observabel create

--- DemoNoSwiftUI/RxObservableViewController.swift
+++ DemoNoSwiftUI/RxObservableViewController.swift
@@ -21,39 +21,42 @@ class RxObservableViewController: UIViewController {
     fatalError("init(coder:) has not been implemented")
   }
  
+  var disposeBag: DisposeBag? = DisposeBag()
   
   override func viewDidLoad() {
-    
-    
-    /// 同步执行
-    let syncObservable = Observable<Int>.create { (observer) -> Disposable in
-        LOG_DEBUG()
-        observer.onNext(1)
-        observer.onCompleted()
-      return Disposables.create()
-    }
-    syncObservable.subscribe()
-    
-    
-    /// async 执行
-    let observable = Observable<Int>.create { (observer) -> Disposable in
-      DispatchQueue.global().async {
-        LOG_DEBUG()
-        observer.onNext(1)
-        observer.onCompleted()
-      }
-      return Disposables.create()
-    }
-    
-    observable.subscribe(onNext: { (value) in
-      LOG_DEBUG()
-    }, onCompleted: {
-      LOG_DEBUG()
-    }) {
-      LOG_DEBUG("end")
-    }
-
-    observable.subscribe()
+    super.viewDidLoad()
+  
   }
   
+  func testCreate() {
+    /// 同步执行
+       let syncObservable = Observable<Int>.create { (observer) -> Disposable in
+           LOG_DEBUG()
+           observer.onNext(1)
+           observer.onCompleted()
+         return Disposables.create()
+       }
+       syncObservable.subscribe()
+       
+       /// async 执行
+       let observable = Observable<Int>.create { (observer) -> Disposable in
+         DispatchQueue.global().asyncAfter(deadline: .now() + .microseconds(1000)) {
+           LOG_DEBUG()
+           observer.onNext(1)
+           observer.onCompleted()
+         }
+         return Disposables.create()
+       }
+
+       let disposable = observable.subscribe(onNext: { (value) in
+         LOG_DEBUG("\(value)")
+       }, onCompleted: {
+         LOG_DEBUG()
+       }) {
+         LOG_DEBUG("end")
+       }
+       disposable.dispose()
+       
+       observable.subscribe()
+  }
 }
