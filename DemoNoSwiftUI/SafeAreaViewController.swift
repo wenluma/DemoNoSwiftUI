d0b9d7f9386c413b170e246c946e6f6e2d6229c9
wenluma <wenluma_521@163.com>
2020-07-07 15:41:30 +0800

    安全区域的高度

--- DemoNoSwiftUI/SafeAreaViewController.swift
+++ DemoNoSwiftUI/SafeAreaViewController.swift
@@ -9,6 +9,12 @@
 import UIKit
 import SnapKit
 
+/*
+ 参考链接 ：
+ 获取安全区域的top，bottom 高度 https://stackoverflow.com/questions/46829840/get-safe-area-inset-top-and-bottom-heights
+ iOS 11, iOS 10 顶部，底部布局view https://svasilevkin.wordpress.com/2019/01/11/snapkit-safe-area-and-layout-guide-difference/
+ */
+
 class SafeAreaViewController: UIViewController {
   override init(nibName nibNameOrNil: String?, bundle nibBundleOrNil: Bundle?) {
     super.init(nibName: nibNameOrNil, bundle: nibBundleOrNil)
@@ -51,22 +57,36 @@ class SafeAreaViewController: UIViewController {
     super.updateViewConstraints()
     
     // 针对 safe area 的出来逻辑 iOS 11+
+    
+    // 顶部安全区域，获取 顶部高度
     topView.snp.makeConstraints { (make) in
       make.top.equalTo(0)
       make.bottom.equalTo(view.safeAreaLayoutGuide.snp.top)
       make.leading.trailing.equalTo(0)
     }
     
+    // 底部安全区域，获得高度
     bottomView.snp.makeConstraints { (make) in
       make.top.equalTo(view.safeAreaLayoutGuide.snp.bottom)
       make.bottom.equalTo(0)
       make.leading.trailing.equalTo(0)
     }
     
+    // 中间内容区域
     contentView.snp.makeConstraints { (make) in
       make.top.equalTo(view.safeAreaLayoutGuide.snp.top)
       make.bottom.equalTo(view.safeAreaLayoutGuide.snp.bottom)
       make.leading.trailing.equalTo(0)
+    }
+    //
+    if #available(iOS 11.0, *) {
+      // let window = UIApplication.shared.keyWindow
+      // 顶部高度
+      let topPadding = view.safeAreaInsets.top
+      // 底部高度
+      let bottomPadding = view.safeAreaInsets.bottom
+      print("topPadding = \(topPadding)")
+      print("bottomPadding = \(bottomPadding)")
     }
   }
   
